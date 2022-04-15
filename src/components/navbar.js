4e473c4f42e5b8dc3e62238797f4c4c736146039
Isaac Sturtridge <eyes330@yahoo.co.uk>
2022-04-15 21:13:44 +0800

    added image

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar(){
     return (
         <nav>
-            <img src="../images/neptune.jpg" />
+            <img src={require('../images/neptune.jpg')} />
             <h3>Planet Facts</h3>
             <h4>React Course - Project 1</h4>
         </nav>
