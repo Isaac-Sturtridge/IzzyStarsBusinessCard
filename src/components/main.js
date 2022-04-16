53f5bc6abdb8583d9e4d95a4bf1b42e1a02c403f
Isaac Sturtridge <eyes330@yahoo.co.uk>
2022-04-16 13:13:31 +0800

    completed first page

--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,15 @@ import React from "react"
 
 export default function Main(){
     return (
-        <h1>Main goes here</h1>
+        <main>
+            <h1 clasName="main--title">Not fun facts about visiting Neptune</h1>
+            <ul className="main--facts">
+                <li>Neptune has no solid surface, meaning you would die by stepping on it</li>
+                <li>Neptune's winds blow at 2,100 kmh, meaning you would die from violent acceleration</li>
+                <li>Neptune is -218C, meaning you would die from mild hypothermia</li>
+                <li>Neptune is 30 times as far from the Sun as Earth is, meaning you would die before seeing those you loved again</li>
+                <li>Neptune is in space. It's hard to go to space if you don't have any money. You would die before saving up enough money to go to Neptune</li>
+            </ul>
+        </main>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Navbar(){
     return (
         <nav>
-            <img src={require('../images/neptune.jpg')} />
-            <h3>Planet Facts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src={require('../images/neptune.jpg')} className="nav--icon" />
+            <h1 className="nav--logo-text">Planet Facts</h1>
+            <h2 className="nav--title">React Course - Project 1</h2>
         </nav>
     )
 }
