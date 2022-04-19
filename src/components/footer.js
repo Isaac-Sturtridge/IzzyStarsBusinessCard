cf4ef65faaee0168364cdd3f775b527c24c30588
Isaac Sturtridge <eyes330@yahoo.co.uk>
2022-04-19 14:09:55 +0800

    completed business card

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Footer(){
     return (
         <footer>
-            
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-linkedin"></a>
+            <a href="#" class="fa fa-github"></a>
+            <a href="#" class="fa fa-youtube"></a>
         </footer>
     )
 }
--- src/components/main.js
+++ src/components/main.js
@@ -3,13 +3,17 @@ import React from "react"
 export default function Main(){
     return (
         <main>
-            <h1 clasName="main--title">Not fun facts about visiting Neptune</h1>
-            <ul className="main--facts">
-                <li>Neptune has no solid surface, meaning you would die by stepping on it</li>
-                <li>Neptune's winds blow at 2,100 kmh, meaning you would die from violent acceleration</li>
-                <li>Neptune is -218C, meaning you would die from mild hypothermia</li>
-                <li>Neptune is 30 times as far from the Sun as Earth is, meaning you would die before seeing those you loved again</li>
-                <li>Neptune is in space. It's hard to go to space if you don't have any money. You would die before saving up enough money to go to Neptune</li>
+            <h1 clasName="main--title">About</h1>
+            <ul className="main--about">
+                <li>BA and MA from University of Exeter, History and International Relations respectively</li>
+                <li>Good writer and talented all-rounder. I enjoy problem solving</li>
+            </ul>
+            <h1 classNmae="main-title">Interests</h1>
+            <ul className="main--about">
+                <li>Computing</li>
+                <li>Music</li>
+                <li>Puzzle Games</li>
+                <li>Foreign Policy</li>
             </ul>
         </main>
     )
